1773eb786599c481edeb9c9ce8855827a13af66e
MyUsername <emanuele.marini37@gmail.com>
2021-09-08 00:28:47 +0200

    rivista ridotta bozza

--- e-shop.php
+++ e-shop.php
@@ -6,6 +6,12 @@
 
 
 /*
+Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online;
+ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l’ereditarietà dove necessario;
+ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Provate a far interagire tra di loro gli oggetti: ad esempio, l’utente dello shop inserisce una carta di credito...
+
 1. creiamo l'eshop
 2. creiamo diversi prodotti
 3. aggiungiamoli all'eshop
@@ -18,14 +24,50 @@
 
 class EShop {
 
-    // mettiamo una lista di prodotti
+    public $name;
+    public $address;
+    public $mail;
+    public $products = [];
+    public $customers = [];
+    public $premiumCustomers = [];
+
+    public function __construct(string $name, string $address, string $mail)
+    {
+          $this->name = $name;
+          $this->address = $address;
+          $this->mail = $mail;
+    }
+
+    public function addProduct(Product $nameProduct)
+    {
+          $this->products[] = $nameProduct;
+    }
+
+    //public function removeProduct(Product $nameProduct)
+    //{
+    //      $this->products[] = $nameProduct;
+    //} ---TODO RIVEDERE ANIMALS MATTINA
+
+    public function getProducts()
+    {
+          return $this->products;
+    }
+
+
 
 }
+
+
 
 // ---------------------------------------------
 
 class Product {
-      
+
+      public $nameProduct;
+      public $category;
+      public $description;
+      public $price;
+
 
 }
 
--- product.php
+++ product.php
@@ -2,22 +2,22 @@
 
 class Product {
 
-      public $id;
+      //public $id;
       public $name;
       public $category;
       public $description;
       public $price;
-      public $packaging;
+      //public $packaging; usa come esterna (boolean)
 
       
 
 }
 //class Product definisce tutte le cartatteristiche del prodotto
 
-
+//copiare diretttamente nell file e-shop  TODO ---riveder uso di public private 
 Class TechProduct extends Product {
 
-      public function __construct(string $name, string $description, int $price, )
+      public function __construct(string $name, string $description, int $price)
       {
             $this->category = "Tech-Product";
             $this->name = $name;
@@ -28,10 +28,10 @@ Class TechProduct extends Product {
       }
 
 }
-
+//copiare diretttamente nell file e-shop  TODO ---riveder uso di public private 
 Class BeautyProduct extends Product {
 
-      public function __construct(string $name, string $description, int $price, )
+      public function __construct(string $name, string $description, int $price)
       {
             $this->category = "Beauty-Product";
             $this->name = $name;
@@ -44,7 +44,7 @@ Class BeautyProduct extends Product {
 
 $martello = new Product();
 
-var_dump($martello);
+
 
 
 ?>
